0a31ff46874063d4adeb2109868e81f00828c06d
Yasrah Kapadia <yasrah.kapadiaa@gmail.com>
2021-10-04 09:23:35 -0500

    Added Dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary(){
@@ -9,7 +10,14 @@ let [keyword, setKeyword] = useState("");
 
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
+        
+
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleResponse(response) {
+        console.log(response.data[0]);
     }
 
     return (
